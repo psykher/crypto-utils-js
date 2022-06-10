64db9ad36f0f8002fa77786f93429677ad90568d
Carlos Alvarez del Castillo Garcia <psykher@gmail.com>
2022-06-10 03:46:49 -0500

    Minor fix

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.base64 = exports.rotx = void 0;
-// ROT-X is a special case of the Caesar cipher
+
 var rot_x_1 = require("./core/rot-x");
 Object.defineProperty(exports, "rotx", { enumerable: true, get: function () { return rot_x_1.rotx; } });
 var base64_1 = require("./core/base64");
